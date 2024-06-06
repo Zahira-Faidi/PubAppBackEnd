f893ed3f0027272cf9727ab50a3924315b7c8219
Zahira-Faidi <faidizahira12@gmail.com>
2024-06-06 09:09:34 +0100

    delete promotion

--- Ads.Application/obj/Debug/net6.0/Ads.Application.AssemblyInfo.cs
+++ Ads.Application/obj/Debug/net6.0/Ads.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ads.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9906a8795966ddec3770d9cbb7bfe17d9171fba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d24550d4cd81c9df628b2878f700a93a75c467")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ads.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ads.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
